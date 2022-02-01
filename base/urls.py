39d0ee1ec693a45fe3f99cc07425a30875e140a8
vrishabml <vrishabm0@gmail.com>
2022-02-01 16:45:39 +0530

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,20 @@
+from os import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
+    path('', views.home, name='home'),
+    path('room/<str:pk>', views.room, name='room'),
+    path('profile/<str:pk>', views.userProfile, name='user-profile'),
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+    path('update-user/', views.updateUser, name='update-user'),
+    path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
+
+]
---
+++ studybud/urls.py
@@ -0,0 +1,10 @@
+from http.client import HTTPResponse
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    path('api/', include('base.api.urls'))
+]
